ce1f7d3d1ba75cf06faf1a493dac76cad11e9c5a
PhMarie <philippe.marie@sous-titre.com>
2022-06-21 09:10:51 +0200

    404 error page

--- 404.php
+++ 404.php
@@ -1,21 +1,18 @@
-<?
-//----- ligne pour générer une erreur 404 si le visiteur est google. En effet google 
-//----- a besoin de trouver une erreur 404 lors de la vérification de site effectue par
-//----- l'outil sitemap.
-if( ereg("^.*Google-Sitemaps.*$", $_SERVER['HTTP_USER_AGENT']) ){ 
-	header("HTTP/1.0 404 Not Found");
-	exit();
-}
-if (file_exists($HTTP_SERVER_VARS["DOCUMENT_ROOT"]."/index.php")) {
-	header("Location: /index.php");
-	exit();
-}
-if (file_exists($HTTP_SERVER_VARS["DOCUMENT_ROOT"]."/index.htm")) {
-	header("Location: /index.htm");
-	exit();
-}
-if(file_exists($HTTP_SERVER_VARS["DOCUMENT_ROOT"]."/home.htm")) {
-	header("Location: /home.htm");
-	exit();
-}
-?>
+<!DOCTYPE html>
+
+<html lang="fr">
+	<head>
+		<!-- Meta Data -->
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>404 Error at Sous-titre.com</title>
+		<meta name="robots" content="no index, no follow">
+		<link rel="stylesheet" href="style.min.css">
+	</head>
+	<body>
+		<?php
+		include 'php/11_404.php';
+		?>
+    </body>
+</html>
--- php/11_404.php
+++ php/11_404.php
@@ -1,16 +1,20 @@
-   <section id="footer" class="dark sectionSeparator text-center" data-stellar-background-ratio="0.6">
+<section id="charts" class="dark">
+    <!-- <div class="black-overlay"></div> -->
+    <div style="text-align:center">
+                <h2><span class="serif">Ooops</span></h2>
+                <h1>Erreur 404</h1>
+                <img class="separator" src="img/separator.png" alt="Separator">
+                <h3>Désolé, nous n'avons pas trouvé la page demandée...</h3>
+                <img class="separator" src="img/separator.png" alt="Separator">
+
+            </div>
+</section>
+   <section style="text-align:center" id="footer" class="dark">
         <div class="topLine">
             <a href="https://sous-titre.com" title="Accueil"><img src="img/webp/white-logo_webAudio.webp" alt="white Logo sous-titre.com web et audio"></a>
         </div>
         <div class="bottomLine">
-            <ul class="footerSocialIcons">
-                <li><a href="https://www.facebook.com/sous.titre.webdesign"><i class="fa fa-facebook fa-2x"></i></a></li>
-                <li><a href="https://twitter.com/SousTitre"><i class="fa fa-twitter fa-2x"></i></a></li>
-                <li><a href="https://instagram.com/aqua_pulko/"><i class="fa fa-instagram fa-2x"></i></a></li>
-            </ul>
             <p>&copy;&nbsp;<?php Print(date("Y"));?> <a href="https://www.sous-titre.com/" >sous-titre.com</a></p>
-            <p style="padding-top:15px"><a href="include/mentions-legales.html#modal" class="js-modal">Mentions légales</a>
-            </p>
             <!--Waves Container-->
             <div>
                 <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -29,4 +33,3 @@
             <!--Waves end-->
         </div>
     </section>
-
