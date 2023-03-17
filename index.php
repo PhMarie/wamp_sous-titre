3efa468753dc629a8d929a8d7ea3aa36bef2a583
PhMarie <philippe.marie@sous-titre.com>
2023-03-17 08:07:46 +0100

    buttons and p text not bold

--- index.php
+++ index.php
@@ -52,8 +52,8 @@
     <!--Styles-->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"> -->
 
-    <link rel="preload" as="style" onload="this.rel='stylesheet'" href="style-20230312.min.css">
-    <link rel="stylesheet" href="style-20230315.min.css">
+    <link rel="preload" as="style" onload="this.rel='stylesheet'" href="style-20230317.min.css">
+    <link rel="stylesheet" href="style-20230317.min.css">
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-93111-3"></script>
     <script>
